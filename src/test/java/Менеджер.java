c8347c2d2017d643cf893cc9042902ea1b800ee4
U405487 <U405487@danfoss.com>
2021-12-02 14:29:16 +0300

    first commit

---
+++ src/test/java/Менеджер.java
@@ -0,0 +1,61 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class Менеджер {
+    public ChromeDriver driver;
+
+
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8080/login");
+        driver.manage().window().maximize();
+    }
+
+
+
+
+
+    @Test
+    public void testLogin() throws InterruptedException {
+
+
+        System.out.println("Enter Email");
+        driver.findElement(By.id("Email")).sendKeys("Admin3@Admin.com");
+        Thread.sleep(1000);
+
+        System.out.println("Enter password");
+        driver.findElement(By.id("password")).sendKeys("Admin@Admin.com");
+        Thread.sleep(1000);
+
+        System.out.println("Click Enter");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/main/div[1]/button/span[1]")).click();
+        Thread.sleep(3000);
+
+        System.out.println("Check url");
+        assertEquals("http://localhost:8080/groups", driver.getCurrentUrl());
+
+        System.out.println("Check title");
+        assertEquals("IndivAMR Cloud", driver.getTitle());
+        Thread.sleep(3000);
+
+        System.out.println("Check menu");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[1]/div/header[2]/div/button")).click();
+        Thread.sleep(3000);
+
+
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+}
+
