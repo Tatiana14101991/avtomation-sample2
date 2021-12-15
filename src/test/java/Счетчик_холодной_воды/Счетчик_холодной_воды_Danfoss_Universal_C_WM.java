9fb59b309959d05c5f2d50bdc9ebf8fbec869cc6
U405487 <U405487@danfoss.com>
2021-12-15 17:30:18 +0300

    first commit

---
+++ src/test/java/Счетчик_холодной_воды/Счетчик_холодной_воды_Danfoss_Universal_C_WM.java
@@ -0,0 +1,127 @@
+package Счетчик_холодной_воды;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static org.junit.Assert.assertEquals;
+
+public class Счетчик_холодной_воды_Danfoss_Universal_C_WM {
+    public ChromeDriver driver;
+
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8080/login");
+        driver.manage().window().maximize();
+    }
+
+
+    @Test
+    public void Danfoss_SonoSafe_10() throws InterruptedException {
+
+
+        System.out.println("Enter Email");
+        driver.findElement(By.id("Email")).sendKeys("Admin@Admin.com");
+        Thread.sleep(1000);
+
+        System.out.println("Enter password");
+        driver.findElement(By.id("password")).sendKeys("Admin@Admin.com");
+        Thread.sleep(1000);
+
+        System.out.println("Click Enter");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/main/div[1]/button/span[1]")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Check url");
+        assertEquals("http://localhost:8080/groups", driver.getCurrentUrl());
+
+        System.out.println("Check title");
+        assertEquals("IndivAMR Cloud", driver.getTitle());
+        Thread.sleep(1000);
+
+        System.out.println("Click Бориса Пастернака,25");
+        Actions actions = new Actions(driver);
+        WebElement elementLocator = driver.findElement(By.xpath("//*[contains(text(), 'Бориса Пастернака,25')]"));
+        actions.doubleClick(elementLocator).perform();
+        Thread.sleep(1000);
+
+        System.out.println("35");
+        elementLocator = driver.findElement(By.xpath(".//tr[3]//td[1][@value='35']"));
+        actions.doubleClick(elementLocator).perform();
+        Thread.sleep(1000);
+
+        System.out.println("Click Добавить");
+        driver.findElement(By.xpath("//*[@title='Добавить']")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Click Тип устройства");
+        driver.findElement(By.xpath("//label[contains(text(),'Тип устройства')]/following::div")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//li[contains(text(),'Счетчик холодной воды')]")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Click Модель");
+        driver.findElement(By.xpath("//label[contains(text(),'Модель')]/following::div")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//li[contains(text(),'Universal C-WM')]")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Click Далее");
+        driver.findElement(By.xpath(".//span[contains(text(), 'Далее')]")).click();
+        Thread.sleep(3000);
+
+        System.out.println("Click Добавить");
+        driver.findElement(By.xpath("//*[@title='Добавить']")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Enter Серийный номер");
+        driver.findElement(By.xpath("//tr[2]/td[1]/div/div/input")).sendKeys("12345678996");
+        Thread.sleep(1000);
+
+        System.out.println("Enter Тип покааний");
+        driver.findElement(By.xpath("//tr[2]/td[9]/div/div/div['Тип показаний']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//li[contains(text(),'Meter status byte')]")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Enter Адаптер");
+        driver.findElement(By.xpath("//tr[2]/td[9]/div/div/div['Тип показаний']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//li[contains(text(),'Meter status byte')]")).click();
+        Thread.sleep(1000);
+
+        System.out.println("Enter Номер канала");
+        driver.findElement(By.xpath("//tr[2]/td[12]/div/div/input")).sendKeys("1");
+        Thread.sleep(1000);
+
+        System.out.println("Click Сохранить");
+        driver.findElement(By.xpath("//button[1][@title='Сохранить']")).click();
+        Thread.sleep(2000);
+
+        System.out.println("Click Сохранить");
+        driver.findElement(By.xpath("//div[3]/div/button[2]/span[1]")).click();
+        Thread.sleep(2000);
+
+        System.out.println("Click Изменить");
+        driver.findElement(By.xpath("//button[2][@title='Изменить']")).click();
+        Thread.sleep(2000);
+
+        System.out.println("Click Удалить");
+        driver.findElement(By.xpath("//button[@title='Удалить']")).click();
+        Thread.sleep(1000);
+
+    }
+
+
+        @After
+    public void afterTest() {
+        driver.quit();
+    }
+}
+
