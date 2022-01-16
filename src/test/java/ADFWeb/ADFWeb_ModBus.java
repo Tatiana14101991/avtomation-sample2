3f96abb2b44f7b8e420bd9452507d170a1e8fe4d
U405487 <U405487@danfoss.com>
2022-01-17 00:43:24 +0300

    first commit

--- src/test/java/ADFWeb/ADFWeb_ModBus.java
+++ src/test/java/ADFWeb/ADFWeb_ModBus.java
@@ -1,5 +1,6 @@
 package ADFWeb;
 
+import lib.Auth;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,19 +24,10 @@ public class ADFWeb_ModBus {
 
 
     @Test
-    public void SonoCollect_110FtpPassive() throws InterruptedException {
+    public void ADFWeb_ModBus() throws InterruptedException {
 
 
-        System.out.println("Enter Email");
-        driver.findElement(By.id("Email")).sendKeys("Admin@Admin.com");
-        Thread.sleep(1000);
-
-        System.out.println("Enter password");
-        driver.findElement(By.id("password")).sendKeys("Admin@Admin.com");
-        Thread.sleep(1000);
-
-        System.out.println("Click Enter");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/main/div[1]/button/span[1]")).click();
+        Auth.login(driver,"Admin@Admin.com","Admin@Admin.com");
 
         Thread.sleep(2000);
         System.out.println("Check url");
@@ -97,13 +89,20 @@ public class ADFWeb_ModBus {
         Thread.sleep(3000);
 
         System.out.println("Check Изменить");
-        driver.findElement(By.xpath("//td[@value='test151']/..//span[text()='edit']")).click();
+        driver.findElement(By.xpath("//td[7]//*[@title='Изменить']")).click();
         Thread.sleep(1000);
 
         System.out.println("Check Удалить");
         driver.findElement(By.xpath("//button[@title=\"Удалить\"]")).click();
         Thread.sleep(1000);
 
+        System.out.println("Check Сохранить");
+        driver.findElement(By.xpath("//*[@title='Сохранить все изменения']")).click();
+        Thread.sleep(3000);
+
+        System.out.println("Check Сохранить все изменения");
+        driver.findElement(By.xpath(".//span[contains(text(), 'Сохранить')]")).click();
+        Thread.sleep(3000);
     }
 
 
